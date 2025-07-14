ff713dafca75023826c576f21e97809eebd74310
Vikuuu <adivik672@gmail.com>
2025-07-14 22:51:37 +0530

    add harper grammar checker

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -70,6 +70,18 @@ return {
 				},
 			})
 
+			lspconfig.harper_ls.setup({
+				filetypes = { "markdown", "tex" },
+				settings = {
+					["harper-ls"] = {
+						linters = {
+							SentenceCapitalization = false,
+							SpellCheck = false,
+						},
+					},
+				},
+			})
+
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
