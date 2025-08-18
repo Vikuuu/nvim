4b1212742331d1e1d082e663c02238d42ef78f08
Vikuuu <adivik672@gmail.com>
2025-08-18 06:02:44 +0530

    remove window navigation keymap
    Interfiering with the <C-l> for clearning the highlight
    of the grep

--- init.lua
+++ init.lua
@@ -28,11 +28,6 @@ vim.keymap.set("i", "<C-H>", "<C-W>")
 vim.keymap.set("n", "<leader>d", ":lua vim.diagnostic.open_float()<CR>")
 -- uppercase the whole word backwords
 vim.keymap.set("i", "<C-u>", "<esc>vbUea")
--- move around the nvim windows
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
 
 require "plugin.colorscheme"
 require "plugin.conform"
