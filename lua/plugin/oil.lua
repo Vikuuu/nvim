48240f9fabb975611ac3989d5941424865a47ec7
Vikuuu <adivik672@gmail.com>
2025-08-30 22:21:11 +0530

    add git status color for oil.nvim

--- lua/plugin/oil.lua
+++ lua/plugin/oil.lua
@@ -1,6 +1,9 @@
 vim.pack.add({
-    "https://github.com/stevearc/oil.nvim"
+    "https://github.com/stevearc/oil.nvim",
+    "https://github.com/benomahony/oil-git.nvim"
 })
 
 require "oil".setup()
 vim.keymap.set("n", "<leader>n", ":Oil<CR>")
+
+require "oil-git".setup()
